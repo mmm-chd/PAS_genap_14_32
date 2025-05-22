08d64aa2214a56bc5e317bc236062a9b0a2b158b
Petrusgld <coysoti@gmail.com>
2025-05-22 09:59:20 +0700

    feat [EM] NavDrawer

--- app/src/main/java/com/intent/pas_genap_14_32/fragment/PremiereLeagueFragment.java
+++ app/src/main/java/com/intent/pas_genap_14_32/fragment/PremiereLeagueFragment.java
@@ -1,4 +1,18 @@
 package com.intent.pas_genap_14_32.fragment;
 
-public class PremiereLeagueFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.intent.pas_genap_14_32.R;
+
+public class PremiereLeagueFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_premiereleague, container, false);
+        return view;
+    }
 }
